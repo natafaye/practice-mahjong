63b759a5222460b60121721353ec76edd6d2a43e
Natalie Childs <nata.faye@gmail.com>
2026-04-21 22:35:13 -0700

    Added 2026 card

--- src/_data/CARDS.ts
+++ src/_data/CARDS.ts
@@ -1,4 +1,5 @@
 import { CARD_2025 } from "./CARD_2025";
+import { CARD_2026 } from "./CARD_2026";
 
-export const CARDS = [CARD_2025]
+export const CARDS = [CARD_2026, CARD_2025]
 export const defaultCard = CARDS[0]
